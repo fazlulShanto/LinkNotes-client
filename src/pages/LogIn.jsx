4e7733cf896c6b7d38d945dce51b2791496c3124
deraijin110 <76022885+deraijin110@users.noreply.github.com>
2024-07-07 17:31:26 +0800

    Login and Signup Page

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { Mail } from 'lucide-react';
+import { LockKeyhole } from 'lucide-react';
 
 export default function LogIn() {
-    return <div>LogIn</div>;
+    return <div className="w-full h-full flex justify-center items-center">
+        <div className=" rounded-xl w-96 h-96 bg-slate-500 text-center">
+            <h1 className="text-3xl my-10">Login</h1>
+            <form action="get">
+                <div className="flex justify-center gap-4 mb-8 bg-slate-100 mx-10 py-2 rounded-xl">
+                    <Mail size={40}/>
+                    <input className="bg-transparent border-0 border-none" type="email" placeholder="Email"/>
+                </div>
+                <div className="flex justify-center gap-4 mb-8 bg-slate-100 mx-10 py-2 rounded-xl">
+                    <LockKeyhole size={40}/>
+                    <input className="bg-transparent border-0 border-none" type="password" placeholder="Password"/>
+                </div>
+                <button className="bg-slate-800 w-32 h-10 rounded-full mb-3 hover:bg-sky-700">Login</button>
+            </form>
+            <p className="text-sm "><i>Forgot password? Click <a href="" className="hover:bg-sky-800">HERE</a></i></p>
+        </div>
+    </div>; 
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { Mail } from 'lucide-react';
+import { LockKeyhole } from 'lucide-react';
 
 export default function SignUp() {
-    return <div>SignUp</div>;
+    return <div className="w-full h-full flex justify-center items-center">
+        <div className=" rounded-xl w-96 h-3/5 bg-slate-500 text-center">
+            <h1 className="text-3xl my-10">Sign Up</h1>
+            <form action="post" className="">
+                <div className="flex justify-center gap-4 mb-8 bg-slate-100 mx-10 py-2 rounded-xl">
+                    <Mail size={40}/>
+                    <input className="bg-transparent border-0 border-none" type="text" placeholder="Name" required/>
+                </div>
+                <div className="flex justify-center gap-4 mb-8 bg-slate-100 mx-10 py-2 rounded-xl">
+                    <Mail size={40}/>
+                    <input className="bg-transparent border-0 border-none" type="email" placeholder="Email" required/>
+                </div>
+                <div className="flex justify-center gap-4 mb-8 bg-slate-100 mx-10 py-2 rounded-xl">
+                    <LockKeyhole size={40}/>
+                    <input className="bg-transparent border-0 border-none" type="password" placeholder="Password" required/>
+                </div>
+                <button className="bg-slate-800 w-32 h-10 rounded-full mb-3 hover:bg-sky-700">Login</button>
+            </form>
+        </div>
+    </div>; 
 }
