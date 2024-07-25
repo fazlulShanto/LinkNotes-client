29d7332ab46d47c94844c1cb026f4d64aeb16337
fazlulShanto <fazlulcse17@gmail.com>
2024-07-25 08:11:09 +0600

    update: remote server url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,8 +2,9 @@ import axios from "axios";
 // export const baseUrl = `http://192.168.0.109:4567`;
 const env = import.meta.env.MODE;
 export const remoteBaseUrl = `http://135.181.141.62:4004`;
+export const remoteRenderUrl = `https://linknotes-server.onrender.com`;
 export const baseUrl =
-    env === "production" ? remoteBaseUrl : `http://localhost:4567`;
+    env === "production" ? remoteRenderUrl : `http://localhost:4567`;
 
 export default axios.create({
     baseURL: baseUrl,
